5d07bed649c17f5205e349a33e2cabd60b92c051
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-08-11 08:33:46 +0900

    use classnames

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
+import classNames from "classnames";
+
 function Button({ children, disabled, onClick }) {
   return (
     <button
-      className={`p-2 text-white ${
+      className={classNames(
+        "p-2 text-white rounded active:bg-blue-700",
         disabled ? "bg-gray-300" : "bg-blue-600"
-      } rounded active:bg-blue-700`}
+      )}
       disabled={disabled}
       onClick={onClick}
     >
