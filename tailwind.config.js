21d27d52b638f9bce9d9455c96e8bd30c5b5090e
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-08-15 13:11:06 +0900

    [ISSUE-9] Add card component and apply to anchor (#10)
    * Add card
    
    * fix: by lint escape quote

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    boxShadow: {
+      card: "0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)",
+    },
+  },
 };
 export const plugins = [];
