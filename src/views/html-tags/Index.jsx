a3b8a65b972713936d474c3e49d7d87b7e0c7679
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-09-29 22:48:10 +0900

    add: Code tag page (#51)

--- src/views/html-tags/Index.jsx
+++ src/views/html-tags/Index.jsx
@@ -64,6 +64,9 @@ function Index() {
             <Link to="/html-tags/cite">{"<cite> Tag"}</Link>
           </li>
           <li>
+            <Link to="/html-tags/code">{"<code> Tag"}</Link>
+          </li>
+          <li>
             <Link to="/html-tags/progress">{"<progress> Tag"}</Link>
           </li>
         </ul>
