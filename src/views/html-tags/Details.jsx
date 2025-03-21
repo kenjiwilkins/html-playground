309cbee0f9566881f5c246d01a6f0c87c62cd4cc
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-03-22 08:23:22 +0900

    fix closed text color (#98)

--- src/views/html-tags/Details.jsx
+++ src/views/html-tags/Details.jsx
@@ -90,10 +90,10 @@ function DetailsPage() {
       <Card>
         <h2 className="text-xl">css styling</h2>
         <details
-          className="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg"
+          className="open:bg-white dark:open:bg-slate-900 open:ring-1 text-slate-900 open:text-white open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg"
           open
         >
-          <summary className="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
+          <summary className="text-sm leading-6 font-semibold select-none">
             {summary}
           </summary>
           <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
